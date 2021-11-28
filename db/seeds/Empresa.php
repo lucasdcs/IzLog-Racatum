798218382aff5aaf1eb43c68365fab9922341d98
Lucas <luucas_dc@hotmail.com>
2021-11-28 09:46:20 -0300

    Atualização em seeds Empresa

--- db/seeds/Empresa.php
+++ db/seeds/Empresa.php
@@ -16,10 +16,14 @@ class Empresa extends AbstractSeed
     public function run()
     {
         $data = [
-            ['empresa' => '775','4499550608'],
-            ['empresa' => 'Samsung','4430303366'],
-            ['empresa' => 'PHILIPS','1120325569'],
-            ['empresa' => 'Nike', '2130237800'],
+            ['empresa' => '775',
+            'whatsapp' => '4499550608'],
+            ['empresa' => 'Samsung',
+            'whatsapp' => '4430303366'],
+            ['empresa' => 'PHILIPS',
+            'whatsapp' => '1120325569'],
+            ['empresa' => 'Nike',
+            'whatsapp' => '2130237800'],
         ];
 
         $empresa = $this->table('empresa');
